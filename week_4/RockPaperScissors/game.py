a95a5c7875e2c5718bfb4deb20b2770f285e94f7
Rachel Kornetsky <rachel.kornetsky@gmail.com>
2026-08-25 20:31:18 +0300

    wk4_day6_dailychallenge

--- week_4/RockPaperScissors/game.py
+++ week_4/RockPaperScissors/game.py
@@ -1,10 +1,12 @@
+
 class Game:
     def get_user_item(self):
         while True:
             user_input = input("Select an item (rock/paper/scissors): ").strip().lower()
             if user_input in ["rock", "paper", "scissors"]:
                 return user_input
-            print("Invalid selection. Please choose rock, paper, or scissors.")
+            else:
+                print("Invalid selection. Please choose rock, paper, or scissors.")
 
     def get_computer_item(self):
         import random
@@ -34,3 +36,20 @@ class Game:
             print("You lose!")
 
         return result
+# get_computer_item(self) – Select rock/paper/scissors at random for the computer. Return the item at the end of the function. Use python’s random.choice() function (read about it online).
+
+# get_game_result(self, user_item, computer_item) – Determine the result of the game.
+# Parameters:
+# user_item – the user’s chosen item (rock/paper/scissors)
+# computer_item – the computer’s chosen (random) item (rock/paper/scissors)
+# Return either win, draw, or loss. Where win means that the user has won, draw means the user and the computer got the same item, and loss means that the user has lost.
+
+# play(self) – the function that will be called from outside the class (ie. from rock-paper-scissors.py). It will do 3 things:
+# Get the user’s item (rock/paper/scissors) and remember it
+
+# Get a random item for the computer (rock/paper/scissors) and remember it
+
+# Determine the results of the game by comparing the user’s item and the computer’s item
+# Print the output of the game; something like this: “You selected rock. The computer selected paper. You lose”, “You selected scissors. The computer selected scissors. You drew!”
+
+# Return the results of the game as a string: win;draw;loss;, where win means that the user has won, draw means the user and the computer got the same item, and loss means that the user has lost.
